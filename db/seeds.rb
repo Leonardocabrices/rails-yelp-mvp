1965728924878563ecbde6b17a4f26754bb1f330
LeonardoParra <leonardocabrices1@gmail.com>
2024-02-23 16:30:04 -0400

    resolucion ejercicio 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+surprise_ingredient = { name: 'Surprise Ingredients', address: '1 Boundary St, Chinese E1 7JE', phone_number: '54782196', category:'chinese'}
+palais_heureux = { name: 'Palais Heureux', address: '2 Boundary St, French E2 7JE', phone_number: '241578469', category:'french'}
+rengoku_rolls = { name: 'Rengoku Rolls', address: '3 Boundary St, Japan E3 7JE', phone_number: '241578468', category:'japanese'}
+belgian_wonder =  { name: 'Belgian Wonder', address: '4 Boundary St, Belgian E4 7JE', phone_number: '241578467', category: 'belgian'}
+luiggi_pizza = { name: 'Luiggi Pizza', address: '5 Boundary St, Italy E5 7JE', phone_number: '241578466', category: 'italian'}
+
+[surprise_ingredient, palais_heureux, rengoku_rolls, belgian_wonder, luiggi_pizza].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
